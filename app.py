b26b07945c85ba504752c31637782f3be55c111f
AndreyCarlos07 <116983710+AndreyCarlos07@users.noreply.github.com>
2026-02-09 07:35:22 -0300

    app.py

--- app.py
+++ app.py
@@ -41,5 +41,6 @@ def vaga(id):
     return "Vaga não encontrada", 404
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
