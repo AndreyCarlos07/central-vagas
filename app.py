0556cfd37e03613b7652229b5d8067bb15e33584
AndreyCarlos07 <116983710+AndreyCarlos07@users.noreply.github.com>
2026-02-09 07:20:12 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask, redirect, render_template_string
+
+app = Flask(__name__)
+
+vagas = {
+    "dados-jr": {
+        "titulo": "Analista de Dados Júnior",
+        "link": "https://www.linkedin.com/jobs/view/123"
+    },
+    "devops-pl": {
+        "titulo": "DevOps Pleno",
+        "link": "https://www.linkedin.com/jobs/view/456"
+    }
+}
+
+@app.route("/")
+def home():
+    html = """
+    <h1>Central de Vagas - Engenharia / Tech</h1>
+    <p>Projeto voluntário para ajudar profissionais a se candidatarem.</p>
+    <ul>
+        {% for key, vaga in vagas.items() %}
+            <li>
+                <a href="/vaga/{{ key }}">{{ vaga.titulo }}</a>
+            </li>
+        {% endfor %}
+    </ul>
+    """
+    return render_template_string(html, vagas=vagas)
+
+@app.route("/vaga/<id>")
+def vaga(id):
+    if id in vagas:
+        return redirect(vagas[id]["link"])
+    return "Vaga não encontrada", 404
+
+if __name__ == "__main__":
+    app.run()
+
