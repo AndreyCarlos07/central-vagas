90db3220f4ca9928f817f9bc775aa2aac1d0bf87
AndreyCarlos07 <116983710+AndreyCarlos07@users.noreply.github.com>
2026-02-18 20:15:42 -0300

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 CSV_FILE = "vagas.csv"
-VAGAS_POR_PAGINA = 100  # ✅ ADICIONADO
+VAGAS_POR_PAGINA = 10  # ✅ ADICIONADO
 
 # ==========================
 # PALAVRAS BLOQUEADAS
