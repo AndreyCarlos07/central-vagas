1c862a16f724acb795952c013296843dba1dfc8b
AndreyCarlos07 <116983710+AndreyCarlos07@users.noreply.github.com>
2026-02-22 10:42:49 -0300

    scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -13,7 +13,7 @@ from datetime import datetime
 # ===========================
 # DEBUG CONFIG
 # ===========================
-MODO_DEBUG = True  # 🔥 Troque para False quando quiser rodar tudo
+MODO_DEBUG = False  # 🔥 Troque para False quando quiser rodar tudo
 EMPRESAS_DEBUG = ["HEINEKEN", "YPE"]
 
 CSV_HISTORICO = "vagas.csv"
