a1a7fe7ac1c564bedc9922aa42028e8f9fac4836
AndreyCarlos07 <116983710+AndreyCarlos07@users.noreply.github.com>
2026-03-09 19:54:10 -0300

    app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ PALAVRAS_BLOQUEADAS = {
     "eusébio",
     "motorista",
     "jaboatão",
-    "técnica ou técnico de segurança do trabalho i - paracatu/mg paracatu - mg efetivo",
+    "paracatu/mg paracatu",
 }
 
 def vagas_ativas():
