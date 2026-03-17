dec287c9ab9ee0679d1ce2875d1b3c3ace410706
AndreyCarlos07 <116983710+AndreyCarlos07@users.noreply.github.com>
2026-03-17 13:00:58 -0300

    scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -14,7 +14,7 @@ from datetime import datetime
 # DEBUG CONFIG
 # ===========================
 MODO_DEBUG = True  # 🔥 Troque para False quando quiser rodar tudo
-EMPRESAS_DEBUG = ["ACELEN", "MDC Energia"]
+EMPRESAS_DEBUG = ["ACELEN", "MDC ENERGIA"]
 
 CSV_HISTORICO = "vagas.csv"
 CSV_NOVAS = "vagas_novas.csv"
