4e053e818dec52c9656446973b2b513e19a22dd0
AndreyCarlos07 <116983710+AndreyCarlos07@users.noreply.github.com>
2026-03-29 13:28:50 -0300

    app.py

--- app.py
+++ app.py
@@ -436,7 +436,7 @@ def home():
 
             <!-- banner_home_topo -->
             <ins class="adsbygoogle"
-                style="display:block; width:100%;"
+                style="display:block; min-width:250px;"
                 data-ad-client="ca-pub-2211390415336582"
                 data-ad-slot="8168087096"
                 data-ad-format="auto"
