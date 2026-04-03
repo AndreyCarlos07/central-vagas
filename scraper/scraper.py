5c6fad3bcd1a73d25f0bbb73e92ab421d41143c3
AndreyCarlos07 <116983710+AndreyCarlos07@users.noreply.github.com>
2026-04-03 08:44:43 -0300

    scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1410,7 +1410,7 @@ def main():
                     vagas = coletar_gerdau(page, site)
 
                 elif site["tipo"] == "petropolis":
-                    vagas = coletar_petropolis(site)
+                    vagas = coletar_petropolis(page, site)
                 
                 elif site["tipo"] == "eightfold":
                     vagas = coletar_eightfold(page, site)
