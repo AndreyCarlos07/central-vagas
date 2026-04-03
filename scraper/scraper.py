40943b5c463df9fca1f41b0094c72683a699ff72
AndreyCarlos07 <116983710+AndreyCarlos07@users.noreply.github.com>
2026-04-03 11:54:39 -0300

    scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -29,7 +29,7 @@ ARQUIVO_BACKUP = "vagas_backup.csv"
 # DEBUG CONFIG
 # ===========================
 MODO_DEBUG = True  # 🔥 Troque para False quando quiser rodar tudo
-EMPRESAS_DEBUG = ["MFX"]
+EMPRESAS_DEBUG = ["MFX", "TRESCAL", "INFOTEC BRASIL"]
 
 CSV_HISTORICO = "vagas.csv"
 CSV_NOVAS = "vagas_novas.csv"
