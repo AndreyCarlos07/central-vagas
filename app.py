f0e9bd38569813cde6bbfd30dc8fb1c41d9b0169
AndreyCarlos07 <116983710+AndreyCarlos07@users.noreply.github.com>
2026-04-18 14:39:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -961,8 +961,7 @@ def pro():
                     {% for empresa in empresas %}
                         <label style="
                             display:flex;
-                            align-items:center;
-                            justify-content:space-between;
+                            align-items:flex-start; /* 👈 muda isso */
                             padding:10px 12px;
                             border-bottom:1px solid #eee;
                             cursor:pointer;
