b7c5fe0b4a406652aafe5894cd1b00fdc2e3aedc
AndreyCarlos07 <116983710+AndreyCarlos07@users.noreply.github.com>
2026-04-30 14:30:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -847,7 +847,7 @@ def contato():
 
                     <select name="tipo">
                         <option value="sugestao">Sugestão</option>
-                        <option value="sugestao">Parcerias</option
+                        <option value="parcerias">Parcerias</option
                         <option value="problema">Problemas no Site</option>
                         <option value="problema_pro">Problemas na Assinatura PRO</option>
                     </select>
@@ -3134,7 +3134,7 @@ def redirect_vaga(id):
         <p>Você está acessando como usuário gratuito.</p>
 
         <p>
-        🚀 Com o plano <b>PRO</b>, você acessa instantaneamente e recebe vagas no email de acordo com seu perfil.
+        🚀 Com o plano <b>PRO</b>, você acessa instantaneamente sem o redirecionamento e recebe vagas no email de acordo com seu perfil.
         </p>
 
         <a href="/pro" style="
